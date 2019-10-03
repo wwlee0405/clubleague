de453aac631b97c34981912cd4ca6a0eb5454b8c
wwlee0405 <wwlee0405@gmail.com>
2019-10-03 22:18:14 +0900

    login

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  StatusBar
+} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar
+        backgroundColor="#1e90ff"
+        barStyle="light-content"
+      />
+      <Text style={styles.welcome}>Login to My App</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <View style={styles.btnContainer}>
+        <TouchableOpacity
+          style={styles.userBtn}
+          onPress={() => alert("Login works")}
+        >
+          <Text style={styles.btnText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.userBtn}
+          onPress={() => alert("Signup works")}
+        >
+          <Text style={styles.btnText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,8 +47,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1e90ff'
   },
-});
+  welcome: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10,
+    color: "#fff"
+  },
+  input: {
+    width: "90%",
+    backgroundColor: "#fff",
+    padding: 15,
+    marginBottom: 10
+  },
+  btnContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "90%"
+  },
+  userBtn: {
+    backgroundColor: "#FFD700",
+    padding: 15,
+    width: "45%"
+  },
+  btnText: {
+    fontSize: 18,
+    textAlign: "center"
+  }
+})
---
+++ src/Routes/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA"
+  },
+  text: {
+    color: "#2C2C2C",
+    fontSize: 30
+  }
+});
